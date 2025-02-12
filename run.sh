79b9b488b51e251b5614bcd88bec39985bd20190
Gxy-2001 <1075586877@qq.com>
2025-02-12 06:40:37 -0800

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+
 conda create -n deepscaler -y python=3.10
 conda init 
 source deactivate
@@ -12,9 +13,16 @@ pip install wandb
 wandb login
 
 
+# # ray start --head --node-ip-address 0.0.0.0 --num-gpus 8
+# # ray start --head --node-ip-address=$(hostname -I | awk '{print $1}') --port=6379 --num-gpus=8
+# ray start --address=10.6.67.173:6379 --num-gpus=8
+
+
+
 cd /scratch/deepscaler
 # 8k context 8 A100-80G
 export VLLM_ATTENTION_BACKEND=XFORMERS
 # Run 8K context length training
 export MODEL_PATH="/mnt/lyna-selfplay/model/DeepSeek-R1-Distill-Qwen-1.5B"
 bash scripts/train/run_deepscaler_1.5b_8k.sh --model $MODEL_PATH
+VLLM_ATTENTION_BACKEND=XFORMERS bash scripts/train/run_deepscaler_1.5b_8k_4node.sh --model $MODEL_PATH
